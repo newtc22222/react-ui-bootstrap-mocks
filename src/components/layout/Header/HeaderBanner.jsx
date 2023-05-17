0230c3cc589f15d122e975a49064126def9a7748
Nhật Phi Võ <nhatphi1598753@gmail.com>
2023-05-17 16:16:40 +0000

    Update HeaderBanner.jsx

--- src/components/layout/Header/HeaderBanner.jsx
+++ src/components/layout/Header/HeaderBanner.jsx
@@ -1,26 +1,44 @@
 import React from "react";
-import { Carousel } from "react-bootstrap";
+import { SpliderCarousel } from "../../common";
+
+const fixedStyleBannerTop = {
+  padding: "0 10%",
+  width: "80%",
+  height: "100%",
+  objectFit: "cover",
+};
 
 const HeaderBanner = () => {
-  const bannerTop = [];
+  const bannerTop = [
+    {
+      render: () => (
+        <img
+          src="https://th.bing.com/th/id/OIP.RmFQYaCHhtm9oQCaTvkNFgHaBB?pid=ImgDet&w=2000&h=276&rs=1"
+          alt=""
+          style={fixedStyleBannerTop}
+        />
+      ),
+    },
+    {
+      render: () => (
+        <img
+          src="https://th.bing.com/th/id/OIP.CrLBdXP8YsyPA9502_s0qQHaA-?pid=ImgDet&rs=1https://th.bing.com/th/id/OIP.CrLBdXP8YsyPA9502_s0qQHaA-?pid=ImgDet&rs=1"
+          alt=""
+          style={fixedStyleBannerTop}
+        />
+      ),
+    },
+  ];
 
   if (bannerTop.length === 0) return <></>;
 
   return (
-    <Carousel>
-      {bannerTop.map((banner, idx) => {
-        return (
-          <Carousel.Item key={idx}>
-            <img
-              className="d-block w-100"
-              src={banner.path}
-              alt={banner.title}
-              onClick={() => {}}
-            />
-          </Carousel.Item>
-        );
-      })}
-    </Carousel>
+    <div className="container">
+      <SpliderCarousel
+        recordList={bannerTop}
+        height={window.innerWidth > 600 ? "7vh" : "5vh"}
+      />
+    </div>
   );
 };
 
